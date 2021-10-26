59b49de9cbe11f0feb23310824db46614578237f
Micheal Limones <micheal@zestylimones.com>
2021-10-25 18:58:01 -0600

    added view js file. added routes.

---
+++ routes/api.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const Workouts = require('../models/workout');
+
+router.get('/api/workouts', (req, res) => {});
+
+router.put('/api/workouts/:id', (req, res) => {});
+
+router.post('/api/workouts', (req, res) => {});
+
+router.get('/api/workouts/range', (req, res) => {});
--- server.js
+++ server.js
@@ -21,6 +21,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 });
 
 // routes
+
+app.use(require('./routes/view.js'));
 app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
